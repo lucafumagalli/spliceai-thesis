0366058d15064553515f249630dcf6ed92820396
Luca <fumagalli.luca96@gmail.com>
2021-10-15 17:15:07 +0200

    added comments to grab_sequence.sh

--- canonical/grab_sequence.sh
+++ canonical/grab_sequence.sh
@@ -6,8 +6,11 @@ CLr=$((CL_max/2))
 CLl=$(($CLr+1))
 # First nucleotide not included by BEDtools
 
+#from canonical_dataset.txt get the interval(first and last position) for each gene
+#substracting and summing the context length and save it in temp.bed
 cat $splice_table | awk -v CLl=$CLl -v CLr=$CLr '{print $3"\t"($5-CLl)"\t"($6+CLr)}' > temp.bed
 
+#extracts sequences from genome.fa file for each of the intervals defined in temp.bed and save them in canonical_sequence.txt
 bedtools getfasta -bed temp.bed -fi $ref_genome -fo $sequence -tab
 
 rm temp.bed
