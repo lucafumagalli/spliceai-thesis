8cae6f35963099f9c2e157d877f5848d6d2b5f88
Luca <fumagalli.luca96@gmail.com>
2021-10-18 10:43:13 +0200

    added hg19 download

--- canonical/create_datafile.sh
+++ canonical/create_datafile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #create datafile for training and testing
+
 python2.7 create_datafile.py train all
 python2.7 create_datafile.py test 0
--- canonical/grab_sequence.sh
+++ canonical/grab_sequence.sh
@@ -2,6 +2,11 @@
 
 source constants.py
 
+if [ ! -f hg19.fa ]; then
+    wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
+    gzip -d hg19.fa.gz
+fi
+
 CLr=$((CL_max/2))
 CLl=$(($CLr+1))
 # First nucleotide not included by BEDtools
@@ -10,7 +15,8 @@ CLl=$(($CLr+1))
 #substracting and summing the context length and save it in temp.bed
 cat $splice_table | awk -v CLl=$CLl -v CLr=$CLr '{print $3"\t"($5-CLl)"\t"($6+CLr)}' > temp.bed
 
-#extracts sequences from genome.fa file for each of the intervals defined in temp.bed and save them in canonical_sequence.txt
+#extracts sequences from genome.fa file for each of the intervals defined in temp.bed
+#and save them in canonical_sequence.txt
 bedtools getfasta -bed temp.bed -fi $ref_genome -fo $sequence -tab
 
 rm temp.bed
